a8f025734f55118a683c91bc6733ce8b11dc5c2d
Mark K Cowan <mark@battlesnake.co.uk>
2015-10-25 02:14:28 +0000

    List cleaner added

---
+++ clean_lists.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -r tmp="$(mktemp)"
+
+cd "$(dirname "$0")"
+
+source "$(dirname "$0")/config"
+
+declare -i count=0
+
+function clean_list {
+	while read line; do
+		if [ "${line:0:1}" == '#' ] || [ -z "${line}" ] || [ -e "${line}" ]; then
+			printf -- "%s\n" "${line}"
+		else
+			printf >&2 -- " * %s\n" "${line}"
+			count+=1
+		fi
+	done
+}
+
+for list in "${list_dir}"/*.list; do
+	clean_list < "${list}" > "${tmp}"
+	cat "${tmp}" > "${list}"
+done
+
+printf -- "%d missing files removed from preload lists\n" "${count}"
+
+rm -f -- "${tmp}"
